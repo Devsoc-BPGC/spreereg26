557cb0386e09928e20fbf400b8f5ec55d4ca8375
Intzaar <sridharvvs1124@gmail.com>
2026-03-20 02:48:35 +0530

    completed the site pt2

--- components/QueriesModal.jsx
+++ components/QueriesModal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const QueriesModal = ({ isOpen, setIsOpen }) => {
   const contacts = [
     {
-      name: "Jehan Daftari",
+      name: "Devansh Wason",
       role: "Spree Controls Events Head",
       phone: "+91 9667289984"
     },
